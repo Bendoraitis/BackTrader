37b7b900b5dda181c00667d360da840237f6d4be
Rimas <rimas@bendoraitis.info>
2024-04-16 19:04:53 +0300

    just to get data, everything else commented

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ import pandas as pd
 start_time = time.time()
 
 
-data_file = download_data.get_merged_coin_data_file("BTCUSDT", '2024-01-26', '2024-01-27')
-average_true_range(data_file, 601, 601)
+data_file = download_data.get_merged_coin_data_file("BTCUSDT", '2017-08-17', '2024-04-15')
+# strategy run
+# average_true_range(data_file, 601, 601)
 
 
 print("\n--- %s seconds ---" % (time.time() - start_time))

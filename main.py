96e15da58174500ac644f73cd0990ff3307c8c92
Rimas <rimas@bendoraitis.info>
2025-02-16 22:09:49 +0200

    created function by_high_volume with copy all code from average_true_range. Next step to add the idea functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from strategy.average_true_range.main import average_true_range
 from strategy.buy_trades_high_level.main import buy_trades_high_level_long_time
 from strategy.LSTM_by_day.main import LSTM_strategy_by_day
 from strategy.Transformer_by_day.main import Transformer_strategy_by_day
+from strategy.by_high_volume.main import by_high_volume
 import time
 import tensorflow as tf
 import os
@@ -23,16 +24,18 @@ start_time = time.time()
 ###############################################################################
 ######                         Load data                                 ######
 ###############################################################################
-data_file = download_data.get_merged_coin_data_file("BTCUSDT", '1m', '2023-10-01', '2023-10-21')
+# data_file = download_data.get_merged_coin_data_file("BTCUSDT", '1m', '2023-10-15', '2023-10-21')
+data_file = download_data.get_merged_coin_data_file("OMUSDT", '1m', '2021-03-08', '2025-02-15')
 
 
 ###############################################################################
 ######                        Strategy run                               ######
 ###############################################################################
-average_true_range(data_file, 601, 601)
-buy_trades_high_level_long_time(data_file)
-LSTM_strategy_by_day(data_file, 500, 100, 6)
-Transformer_strategy_by_day(data_file, 500, 100, 2)
+# average_true_range(data_file, 601, 601)
+# buy_trades_high_level_long_time(data_file)
+# LSTM_strategy_by_day(data_file, 500, 100, 6)
+# Transformer_strategy_by_day(data_file, 500, 100, 2)
+by_high_volume(data_file, 601, 601)
 
 print("\n--- %s seconds ---" % (time.time() - start_time))
 print("--- %s minutes ---" % ((time.time() - start_time) / 60))
